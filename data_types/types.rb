9c76cbd34b006237e58fcc1db34e37a07ac3f69b
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-21 03:59:46 -0300

    Adicionado arquivo data types

---
+++ data_types/types.rb
@@ -0,0 +1,30 @@
+# adittion
+
+18 + 7 #->25
+
+# subtraction
+
+72 -7 #->65
+
+# multiplication
+
+7 * 7 #->49
+
+# division
+
+72 / 7 #->10
+
+# exponent
+
+7 ** 2 #->49
+
+# modulus 
+
+7 % 2 #->1
+
+# converting number types
+
+18.to_f #->18.0
+
+18.0.to_i #18
+
