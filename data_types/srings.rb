3ce278e399b707b95f624bb0f22ebfb7fc28b8e6
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-21 04:32:39 -0300

    Adicionando arquivo de strings

---
+++ data_types/srings.rb
@@ -0,0 +1,20 @@
+# Concatenation
+
+# operation plus
+
+"Welcome" + "to" + "Menandro!" #-> Welcome to Menandro!
+
+# operator shovel 
+
+"Welcome " << "to" << "Menandro!" #-> Welcome to Menandro!
+
+#concat method
+
+"welcome ".concat("to").concat("Menandro!") #-> Welcome to Menandro!
+
+# Substring
+
+"hello"[0] #-> "h"
+"hello"[0..1] #-> "he"
+"hello"[0,4] #-> "hell"
+"hello"[-1] #-> "o"
