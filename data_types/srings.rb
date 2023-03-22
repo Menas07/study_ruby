4fb590dcf566980063a48378ba10396e3b17cc35
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-22 04:26:02 -0300

    Atualização

--- data_types/srings.rb
+++ data_types/srings.rb
@@ -18,3 +18,41 @@
 "hello"[0..1] #-> "he"
 "hello"[0,4] #-> "hell"
 "hello"[-1] #-> "o"
+
+#Interpolation 
+
+name = "Menandro"
+puts "Hello, #{name}" #-> Hello, Menandro
+puts 'Hello, #{name}' #-> Hello, #[name}
+
+#Capitalize
+
+"Hello".capitalize #-> "Hello"
+
+#Include 
+
+"Hello".include?("lo")
+"Hello".include?("w")
+
+#upcase
+
+"hello".upcase #-> "HELLO"
+
+#downcase
+
+"HELLO".downcase #->"hello"
+
+#lenght
+
+"hello".lenght #->5
+
+#reverse
+
+"hello".reverse #->olleh
+
+#split
+
+"hello world".split #-> ["hello", "world"]
+"hello".split("") #-> ["h","e","l","l","o"]
+
+
