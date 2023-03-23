00ee7ee699b88a6b7b5cd22d29433cddf2ae5083
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-23 04:51:36 -0300

    Adicionando nova pasta e arquivo

---
+++ variables/declaring_variable.rb
@@ -0,0 +1,13 @@
+age = 18 #-> 18
+age = 18 + 5 #-> 23
+age = 18 #-> 18 
+age = age + 4 #-> 22
+age = 18 #-> 18
+age += 4 #-> 22
+age = 18 # => 18
+age -=2 #-> 16
+cash = 10 #=> 10
+cash *= 2 #-> 20
+temperature 40 #-> 40
+temperature /= 10 #=> 4
+
