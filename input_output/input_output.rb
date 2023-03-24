745351bd87eeb1db5bcdc984f5f334468ae1c00d
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-24 04:24:56 -0300

    Adicionando nova pasta e arquivo

---
+++ input_output/input_output.rb
@@ -0,0 +1,11 @@
+puts "Learning to code is cool!"
+
+puts "Hey, I want 2 key line ples"
+
+x = "My name is ALEX "
+    puts x
+
+    new _string = gets.chomp
+    This is a sentence
+    puts new_string
+    
