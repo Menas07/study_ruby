efcf1f8353207547bf33d0a872143c1d5140557f
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-25 10:31:48 -0300

    Adicionando arquivo de condicionais

---
+++ conditional/conditional.rb
@@ -0,0 +1,25 @@
+if 1 < 2 
+    puts "Hot diggtly, 1 is less than 2!"
+end
+
+#-> hot diggity, 1 is less than 2!
+
+a = 5
+b = 5 
+a.equal?(b) #->true
+
+a = "hello"
+b = "hello"
+a.equal?(b) #->false
+
+if 1 < 2 && 5 < 6
+    puts "Party at Kevin's"
+end
+
+if 10 < 2 or 5 < 6
+    puts "Party at Kevin's!"
+end
+
+if !false #-> true
+
+if !(10< 5) #->true    
