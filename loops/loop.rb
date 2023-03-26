526ed7dfae5cb1d35b37799c375d717c754e6c54
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-26 10:26:01 -0300

    Adicionando pasta e arquivo sobre laços

---
+++ loops/loop.rb
@@ -0,0 +1,42 @@
+# Loop
+
+i = 0
+loop do 
+    puts "i is #{i}"
+    i += 1
+    break if i == 10
+end
+
+#While loop
+
+i = 0 
+while i < 10 do
+    puts " i is #{i}"
+    i += 1
+end
+
+# Until Loop
+
+i = 0 
+until i >= 10 do
+    puts "i is #{i}"
+    i += 1
+end
+
+# For Loop 
+
+for i in 0..5
+    puts "#{i} zombies incoming"
+end
+
+# Times Loop
+
+5.times do 
+    puts "Hello, world"
+end
+
+
+
+
+
+
