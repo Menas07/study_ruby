e03d1e5b945ea9fd69a086407a982cd55ceeef54
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-27 04:53:24 -0300

    Atualizando arquivo

--- Array/array.rb
+++ Array/array.rb
@@ -14,3 +14,27 @@ str_array[4] #-> "array"
 str_array[-1] #-> "array"
 str_array[-2] #-> "small"
 
+str _array = ["This", "is", "a", "small", "array"]
+
+str_array.first #-> "this"
+str_array.first(2) #-> ["This", "is"]
+str_array.last(2) #-> ["small", "array"]
+
+num_array = [1, 2]
+
+num_array.push(3, 4) #-> [1, 2, 3, 4]
+num_array << 5 #-> [1, 2, 3, 4, 5]
+num_array.pop #-> 5
+num_array #-> [1, 2, 3, 4]
+
+num_array = [2, 3, 4]
+
+num_array.unshift(1) #-> [1, 2, 3, 4]
+num_array.shift #-> 1
+num_array #-> [2, 3, 4]
+
+a = [1, 2, 3]
+b = [3, 4, 5]
+
+a + b #-> [1, 2, 3, 4, 5]
+a.concat(b) #-> [1, 2, 3, 4, 5]
