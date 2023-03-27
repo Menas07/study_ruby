1aeb1ff8d2f0d92fb4b7a6b8bd049a0110a2c17d
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-27 04:19:45 -0300

    Adicioando pasta e arquivo sobre arrays

---
+++ Array/array.rb
@@ -0,0 +1,16 @@
+num_array = [1,2,3,4,5]
+str_array = ["this", "is", "a". "small"."array"]
+
+Array.new #-> []
+Array.new(3) #-> [n1, n2, n3]
+Array.new(3,7) #-> [7,7,7]
+Array.new(3,true) #->[true,true,true]
+
+str_array = ["This", "is", "a", "small","array"]
+
+str_array[0] #-> "this"
+str_array[1] #-> "is"
+str_array[4] #-> "array"
+str_array[-1] #-> "array"
+str_array[-2] #-> "small"
+
