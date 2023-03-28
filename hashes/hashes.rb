2075f5778539de404f1e3e6e1cc52ecf92f05b23
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-28 04:07:41 -0300

    Adicionando pasta e arquivo sobre hash

---
+++ hashes/hashes.rb
@@ -0,0 +1,16 @@
+my_hash = {
+    "a random word" => "ahoy"
+    "Dorothy's math test score" => 94, 
+    "an array" => [1, 2, 3],
+    "anempty hash within a hash" =>[]
+}
+
+hash = {9 => "nine", six => 6}
+
+shoes = {
+    "summmer" => "sandals"
+    "winter" => "boots"
+}
+
+shoes["summer"] #-> "sandals"
+
