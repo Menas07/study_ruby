b2f969225b8a6d664bdc30946a13bb42f1d3a760
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-28 04:55:27 -0300

    Atualização do arquivo

--- hashes/hashes.rb
+++ hashes/hashes.rb
@@ -14,3 +14,26 @@ shoes = {
 
 shoes["summer"] #-> "sandals"
 
+shoes ["fall"] = "sneakers"
+
+shoes #-> {"summer"=>"sandals", "winter"=>"boots", "fall"=>"sneakers" }
+
+shoes["summer"] = "flip-flop"
+shoes #-> {"summer"=>"flip-flop", "winter"=> "boots", "fall"=>"sneakers" }
+
+shoes.delete("summer") #-> "flip-flop"
+shoes #-> {"winter" => "boots", "fall" =>"sneakers"}
+
+books = {
+    "infinite jest" => "David Foster Wallace"
+    "into the wild" => "Jon Krakauer"
+
+}
+
+books.keys #-> ["infinite jest", "into the wild"]
+books.valus #-> ["David Foster Wallace", "Jon Krakauer"]
+
+hash1 = {"a" => 100, "b" => 200}
+hash2 = {"b" => 254, "c" => 300}
+hash1.merge(hash2) #-> {"a" => 100, "b" => 254, "c" => 300}
+
