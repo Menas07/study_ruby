05f02f2ab49abb77dd672a63fd8824bb90845194
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-30 04:54:17 -0300

    Correção de uma linha

--- hashes/hashes.rb
+++ hashes/hashes.rb
@@ -15,6 +15,32 @@ shoes = {
     shoes["fall"] = "sneakers"
 
     shoes #-> {"summer" =>"sandals", "winter"=>"boots", "fall"=>"sneakers"}
-    
 
+shoes["summer"] = "flip-flop"
+shoes #-> {"summer"=>"flip flop", "winter"=>"boots", "fall"=>"sneakers"}
+
+shoes.delete("summer") #-> "flip-flop"
+shoes #-> {"winter"=>"boots", "fall"=>"sneakers"}
+
+books = { 
+    "Infinite Jest" => "David Foster Wallaca"
+    "Into the Wild" => "Jon Krakauer"
+
+}
+
+books.keys #-> ["Infinite Jest ", "Into the Wild"]
+books.value #-> ["David Foster Wallace", "Jon Krakauer"]
+
+hash1 = {"a" => 100, "b" => 200}
+hash2 = {"b" => 254, "c" => 300}
+
+hash1.merge(hash2) #-> {"a" => 100, "b" => 254, "c" => 300}
+
+# Symbols Syntax
+japanase_cars = {
+    honda: "Accord",
+    toyota: "corolla",
+    nissan: "altima"
+
+}
 
