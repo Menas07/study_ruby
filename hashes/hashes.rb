803b580a168d7afa357e567b47f7aa7848025389
Menandro Andrade <menandroandrade18@gmail.com>
2023-03-30 04:34:20 -0300

    atualizando arquivo

--- hashes/hashes.rb
+++ hashes/hashes.rb
@@ -1,3 +1,20 @@
+my_hash = {
+    " a random word" => "ahoy",
+    "Dorothy math test score" => 94,
+    "an array" => [1, 2, 3],
+    "an empty hash within a hash" => {}
+}
 
+shoes = {
+    "summer" => "sandasls",
+    "winter" => "boots",
+    }
+
+    shoes["summer"] #-> "sandals"
+
+    shoes["fall"] = "sneakers"
+
+    shoes #-> {"summer" =>"sandals", "winter"=>"boots", "fall"=>"sneakers"}
+    
 
 
